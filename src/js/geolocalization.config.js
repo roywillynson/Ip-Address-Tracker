9672b883637c6fd80e41125e3ea741c187cd6a51
Roy Willynson <roywillinsondelorbemedina@gmail.com>
2020-10-11 02:58:17 -0400

    Update

---
+++ src/js/geolocalization.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  apiKey: "at_0O9aZMyoJW4aTWCtHxhanQWotQOgS",
+  url: "https://geo.ipify.org/api/v1",
+};
---
+++ src/js/map.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  accessToken:
+    "pk.eyJ1Ijoicm95d2lsbGluc29uIiwiYSI6ImNrZmE1NnpsZzA1OWcycW9ndjR6bGNma3QifQ.XrI-V0EQUQcsyMxlxGYfLg",
+  attribution:
+    'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  id: "mapbox/streets-v11",
+};
